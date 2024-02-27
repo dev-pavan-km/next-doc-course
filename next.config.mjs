e5a90381de60b15aec7341934dfc4b375a51f17a
dev.pavan.km <dev.pavan.km@gmail.com>
2024-02-27 06:51:08 +0530

    added bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzerPlugin = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withBundleAnalyzerPlugin(nextConfig);
